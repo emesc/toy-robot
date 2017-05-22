95272eee4d2bff880d398f12ca93e59f9a1396fb
emesc <tahder77@gmail.com>
2017-05-22 18:43:08 +1000

    Added method for moving towards current direction.

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,13 +1,33 @@
+require_relative 'position'
+
 class Robot
   attr_accessor :x, :y, :f
 
   def initialize(position)
-    @x = position.x
-    @y = position.y
-    @f = position.f
+    @x = position.x || 0 
+    @y = position.y || 0
+    @f = position.f || 'north'
   end
 
   def move
-    
+    self.send("go_#{f}".to_sym)
   end
+
+  private
+
+    def go_north
+      @y += 1
+    end
+
+    def go_south
+      @y -= 1
+    end
+
+    def go_east
+      @x += 1
+    end
+
+    def go_west
+      @x -= 1
+    end
 end
