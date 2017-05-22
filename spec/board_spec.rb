4f3ee056b801254cec0572ab70059fd90e25c9b5
emesc <tahder77@gmail.com>
2017-05-22 12:22:51 +1000

    Modified to test class methods.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,9 +1,8 @@
 require 'board'
 
 RSpec.describe Board do
-  board = Board.new
 
-  specify{ expect(board.width).to eq 5 }
+  specify{ expect(Board.width).to eq 5 }
 
-  specify{ expect(board.height).to eq 5 } 
+  specify{ expect(Board.height).to eq 5 } 
 end
