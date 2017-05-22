f5d997837c035b7cd294836f648be45fbad29676
emesc <tahder77@gmail.com>
2017-05-22 11:03:28 +1000

    Modified to instantiate a new robot.

--- spec/position_spec.rb
+++ spec/position_spec.rb
@@ -1,12 +1,13 @@
 require 'position'
+require 'robot'
 
 RSpec.describe Position do
-  let(:position) { Position.new(1, 1, 'south') }
-  let(:robot) { Robot.new }
+  let(:position) { Position.new(x: 1, y: 1, f: 'south') }
   
   it "places the robot on the desired position" do
+    robot = Robot.new(position)
     expect(robot.x).to eq position.x
     expect(robot.y).to eq position.y
-    expect(robot.z).to eq position.z  
+    expect(robot.f).to eq position.f
   end
 end
