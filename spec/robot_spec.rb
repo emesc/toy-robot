440c50664703b7754555b719d882a8dd84f880c0
emesc <tahder77@gmail.com>
2017-05-22 20:28:09 +1000

    Test for moving out of the board edges.

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -47,15 +47,55 @@ RSpec.describe Robot do
       end
     end
 
-    context "out from the edges" do
+    context "out from the top right corner" do
+      let(:position){ Position.new(x: Board.width - 1, y: Board.height - 1, f: 'north') }
 
-      it "should stay in place when moved out of the top right corner" do
-        position.x = Board.width - 1
-        position.y = Board.height - 1
-        position.f = 'north'
-        expect{ robot.move }.not_to change{ robot.x }
-        expect{ robot.move }.not_to change{ robot.y }
-        expect{ robot.move }.not_to change{ robot.f }
+      it "should stay in place when moved up" do
+        expect{ robot.move }.not_to change{ robot }
+      end
+
+      it "should stay in place when moved right" do
+        position.f = 'east'
+        expect{ robot.move }.not_to change{ robot }
+      end
+    end
+
+    context "out from the top left corner" do
+      let(:position){ Position.new(x: 0, y: Board.height - 1, f: 'north') }
+
+      it "should stay in place when moved up" do
+        expect{ robot.move }.not_to change{ robot }
+      end
+
+      it "should stay in place when moved left" do
+        position.f = 'west'
+        expect{ robot.move }.not_to change{ robot }
+      end
+    end
+
+    context "out from the bottom right corner" do
+      let(:position){ Position.new(x: Board.width - 1, y: 0, f: 'south') }
+
+      it "should stay in place when moved down" do
+        expect{ robot.move }.not_to change{ robot }
+      end
+
+      it "should stay in place when moved right" do
+        position.f = 'east'
+        expect{ robot.move }.not_to change{ robot }
+      end
+    end
+
+    context "out from the bottom left corner" do
+      let(:position){ Position.new(x: 0, y: 0, f: 'south') }
+
+      it "should stay in place when moved down" do
+        expect{ robot.move }.not_to change{ robot }
+      end
+
+      it "should stay in place when moved left" do
+        position.f = 'west'
+        expect{ robot.move }.not_to change{ robot }
       end
     end
   end
