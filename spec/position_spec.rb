ecf3e1a916b78d7066e479a774ead90e7a6f073d
emesc <tahder77@gmail.com>
2017-05-22 11:10:53 +1000

    Test for valid positions.

--- spec/position_spec.rb
+++ spec/position_spec.rb
@@ -3,11 +3,16 @@ require 'robot'
 
 RSpec.describe Position do
   let(:position) { Position.new(x: 1, y: 1, f: 'south') }
+  let(:robot) { Robot.new(position) }
   
-  it "places the robot on the desired position" do
-    robot = Robot.new(position)
+  it "places the robot on the desired position" do   
     expect(robot.x).to eq position.x
     expect(robot.y).to eq position.y
     expect(robot.f).to eq position.f
+  end
+
+  it "ensures the position of the robot is within the board dimensions" do
+    expect(robot.x).to be_valid_x
+    expect(robot.y).to be_valid_y
   end
 end
