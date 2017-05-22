dde4156fcbf25dc3db51e04838999229a1bef403
emesc <tahder77@gmail.com>
2017-05-22 18:42:32 +1000

    Test for unrestrained move.

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -2,8 +2,7 @@ require 'robot'
 require 'position'
 
 RSpec.describe Robot do
-
-  let(:position) { Position.new(x: 0, y: 0, f: 'north') }
+  let(:position) { Position.new(x: 3, y: 3, f: 'west') }
   let(:robot) { Robot.new(position) }
 
   context "initial placement" do
@@ -12,6 +11,39 @@ RSpec.describe Robot do
       expect(robot.x).to eq position.x
       expect(robot.y).to eq position.y
       expect(robot.f).to eq position.f
+    end
+  end
+
+  describe "#move" do
+
+    context "without constraints" do
+
+      it "should move 1 unit to the west when facing west" do
+        expect{ robot.move }.to change{ robot.x }.by -1
+        expect{ robot.move }.not_to change{ robot.y }
+        expect{ robot.move }.not_to change{ robot.f }
+      end
+
+      it "should move 1 unit to the east when facing east" do
+        position.f = 'east'
+        expect{ robot.move }.to change{ robot.x }.by 1
+        expect{ robot.move }.not_to change{ robot.y }
+        expect{ robot.move }.not_to change{ robot.f }
+      end
+
+      it "should move 1 unit to the north when facing north" do
+        position.f = 'north'
+        expect{ robot.move }.not_to change{ robot.x }
+        expect{ robot.move }.to change{ robot.y }.by 1
+        expect{ robot.move }.not_to change{ robot.f }
+      end
+
+      it "should move 1 unit to the south when facing south" do
+        position.f = 'south'
+        expect{ robot.move }.not_to change{ robot.x }
+        expect{ robot.move }.to change{ robot. y}.by -1
+        expect{ robot.move }.not_to change{ robot.f }
+      end
     end
   end
 end
