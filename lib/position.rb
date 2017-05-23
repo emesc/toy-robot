e094c9666188ac3665912e7776520521a133a5ce
emesc <tahder77@gmail.com>
2017-05-23 15:16:20 +1000

    Fixed maximum board positions.

--- lib/position.rb
+++ lib/position.rb
@@ -10,10 +10,10 @@ class Position
   end
 
   def valid_in_x?(x)
-    (0..Board.width).include? x
+    (0..Board.width-1).include? x
   end
 
   def valid_in_y?(y)
-    (0..Board.height).include? y
+    (0..Board.height-1).include? y
   end
 end
